f7e29b45fc80974fdc9577e4749074d58793f5b5
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-10-17 16:08:49 +0200

    More penalty on opponent option count

--- ReversiAI.hs
+++ ReversiAI.hs
@@ -406,7 +406,7 @@ niceValue board pos player
  -}
 boardValue :: [Cell] -> Player -> Int
 boardValue board player =
-  - length (options board (opponent player)) * 2
+  - length (options board (opponent player)) * 3
     + foldl
       ( \acc (pos, state) ->
           if owns player (pos, state)
